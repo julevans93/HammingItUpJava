b03d49d027bec65e9acc8fc8b1f0fb4b4a98ff12
Julia Evans <julevans93@users.noreply.github.com>
2019-11-17 11:40:37 -0500

    complete

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
 package zipcode;
 
 public class Hamming {
+    private String string1;
+    private String string2;
+
     public Hamming(String s, String s1) {
+        this.string1 = s;
+        this.string2 = s1;
+
+        if (string1.length() != string2.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int counter = 0;
+        for (int i = 0; i < string1.length(); i++){
+                if (string1.charAt(i) != string2.charAt(i)){
+                    counter++;
+            }
+        }
+        return counter;
     }
 }
